f2628e712d7f1d52896f8744ad496a9edc8829ee
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-04-23 01:03:40 +0300

    Add more socket extensions method

--- Packages/com.cdm.imobiledevice/Runtime/DeviceSocketExtensions.cs
+++ Packages/com.cdm.imobiledevice/Runtime/DeviceSocketExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace iMobileDevice.Unity
@@ -84,6 +85,70 @@ namespace iMobileDevice.Unity
         public static double? ReceiveDouble(this IDeviceSocket socket)
         {
             return socket.Receive(BitConverter.ToDouble);
+        }
+        
+        public static async Task<bool> SendBufferAsync(this IDeviceSocket socket, byte[] buffer, int length = -1)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
+
+            if (buffer.Length < length)
+                throw new ArgumentOutOfRangeException(nameof(length));
+            
+            if (length < 0)
+            {
+                length = buffer.Length;
+            }
+            
+            if (await socket.SendInt32Async(length) &&
+                await socket.SendAsync(buffer, length) == length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static async Task<byte[]> ReceiveBufferAsync(this IDeviceSocket socket)
+        {
+            var length = await socket.ReceiveInt32Async();
+            if (length.HasValue)
+            {
+                var bytes = new byte[length.Value];
+                if (await socket.ReceiveAsync(bytes, bytes.Length) == bytes.Length)
+                {
+                    return bytes;
+                }
+            }
+
+            return null;
+        }
+
+        public static async Task<bool> SendStringAsync(this IDeviceSocket socket, string content)
+        {
+            return await SendStringAsync(socket, content, Encoding.UTF8);
+        }
+
+        public static async Task<bool> SendStringAsync(this IDeviceSocket socket, string content, Encoding encoding)
+        {
+            var bytes = encoding.GetBytes(content);
+            return await socket.SendBufferAsync(bytes);
+        }
+
+        public static async Task<string> ReceiveStringAsync(this IDeviceSocket socket)
+        {
+            return await ReceiveStringAsync(socket, Encoding.UTF8);
+        }
+
+        public static async Task<string> ReceiveStringAsync(this IDeviceSocket socket, Encoding encoding)
+        {
+            var bytes = await socket.ReceiveBufferAsync();
+            if (bytes != null)
+            {
+                return encoding.GetString(bytes);
+            }
+            
+            return string.Empty;
         }
 
         private static bool Send<T>(this IDeviceSocket socket, T value , ConvertToBytesDelegate<T> convert) 
